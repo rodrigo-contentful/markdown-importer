27bc770231d67a1092ce8cb107c62982dd1141b5
Rodrigo Casado <rodrigo.casado@contentful.com>
2022-07-29 08:46:56 +0200

    validations for markdown files and Forma36 styling

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form,Button } from '@contentful/f36-components';
+import { Form,Button, FormControl,TextInput } from '@contentful/f36-components';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 import 'codemirror/lib/codemirror.css';
 import {  MarkdownEditor } from '@contentful/field-editor-markdown';
@@ -8,10 +8,19 @@ const Field = () => {
   const sdk = useSDK();
   sdk.window.startAutoResizer();
   const [file, setFile] = useState()
+  const [filename, setFilename] = useState('')
 
   // handle the upload of a file into a state.
   function handleChange(event) {
-    setFile(event.target.files[0])
+    let fname = event.target.files[0];
+    
+    var re = /(\.mdown|\.mkdn|\.md|\.mkd|\.mdwn|\.mdtxt|\.mdtext|\.text)$/i;
+    if (!re.exec(fname.name)) {
+      setFilename('')
+    } else {
+      setFile(fname)
+      setFilename(fname.name)
+    }
   }
   
   // handle the submit of the form, reads the file and add the content
@@ -26,11 +35,25 @@ const Field = () => {
    
   }
 
+
   return (<>
     <Form onSubmit={handleSubmit}>
-      <input type="file" onChange={handleChange}/>
-      <br/>
-      <br/>
+    <FormControl isInvalid={!filename}>
+      <FormControl.Label>Upload markdown file</FormControl.Label>
+      <TextInput
+         defaultValue=""
+        type="file"
+        name="file"
+        placeholder="test-markdown.md"
+        onChange={handleChange}
+      />
+      <FormControl.HelpText>Select a markdown file to import below</FormControl.HelpText>
+      {!filename && (
+        <FormControl.ValidationMessage>
+          Files supported: .markdown, .mdown, .mkdn, .md, .mkd, .mdwn, .mdtxt, .mdtext and .text
+        </FormControl.ValidationMessage>
+      )}
+      </FormControl>
       <Button variant="primary" type="submit">Upload</Button>
     </Form>
     <MarkdownEditor isInitiallyDisabled={false} sdk={sdk}/>
